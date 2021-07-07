fd6109ccbaebc3e68c481617e76910c9a5d99b09
Pasha <Pashakhurtenko@gmail.com>
2021-07-07 19:39:03 +0300

    s

--- battle_one.py
+++ battle_one.py
@@ -39,10 +39,40 @@ class Fighter():
         self.start_potions = potions
         self.potions = potions
         self.alive = True
-        img = pygame.image.load(f'img/{self.name}/Idle/0.png')
-        self.image = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0#:idle, 1:attack, 2:hurt, 3:dead
+        self.update_time = pygame.time.get_ticks()
+        #load_images
+        temp_list = []
+        for i in range(8):
+            img = pygame.image.load(f'img/{self.name}/Idle/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        #load attack images
+        temp_list = []
+        for i in range(8):
+            img = pygame.image.load(f'img/{self.name}/Attack/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+    def update(self):
+        animation_cooldown = 100
+        #handle animation
+        #update image
+        self.image = self.animation_list[self.action][self.frame_index]
+        #check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        #if the animation has run out reset back to the start
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
 
     def draw(self):
         screen.blit(self.image, self.rect)
@@ -64,8 +94,10 @@ while run:
     draw_panel()
 
     #draw figters
+    knight.update()
     knight.draw()
     for bandit in bandit_list:
+        bandit.update()
         bandit.draw()
 
     for event in pygame.event.get():
